223d5b7c28811acad07d57742eecc060d3e16f9f
csbaobao <xuantin4@ualberta.ca>
2026-01-28 21:31:45 -0700

    Change default color of Shape from blue to green

--- app/src/main/java/ca/ualberta/cs/haochenjia/lab_04/Shape.java
+++ app/src/main/java/ca/ualberta/cs/haochenjia/lab_04/Shape.java
@@ -3,8 +3,7 @@ package ca.ualberta.cs.haochenjia.lab_04;
 public abstract class Shape {
     protected int x;
     protected int y;
-
-    String color = "blue";
+    String color = "green";
 
     public Shape(int x, int y) {
         this.x = x;
