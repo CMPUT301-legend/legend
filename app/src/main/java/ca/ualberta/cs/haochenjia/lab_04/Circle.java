a120ac3086598581cade3db2fa8dd8a0765d1491
Hongchen Li <hongche4@ualberta.ca>
2026-01-28 20:29:40 -0700

    Add Circle model class

---
+++ app/src/main/java/ca/ualberta/cs/haochenjia/lab_04/Circle.java
@@ -0,0 +1,9 @@
+package ca.ualberta.cs.haochenjia.lab_04;
+
+public class Circle extends Shape {
+    private int r;
+    public Circle(int x, int y, int r) {
+        super(x, y);
+        this.r = r;
+    }
+}
